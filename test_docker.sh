5cfa9fcfd06ca95bb9bcc98eacf32f0e39b6d68a
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-11-23 09:00:17 -0500

    Added docker test script

---
+++ test_docker.sh
@@ -0,0 +1,28 @@
+docker run \
+--mount type=bind,src=`pwd`/INPUTS,dest=/INPUTS \
+--mount type=bind,src=`pwd`/OUTPUTS,dest=/OUTPUTS \
+gradtensor \
+/opt/gradtensor/bin/run_fieldmaps_to_gradtensor.sh \
+/usr/local/MATLAB/MATLAB_Runtime/v92 \
+fieldmap_hz_0_file /INPUTS/B0_0_Hz.nii.gz \
+fieldmap_hz_X_file /INPUTS/B0_X_Hz.nii.gz \
+fieldmap_hz_Y_file /INPUTS/B0_Y_Hz.nii.gz \
+fieldmap_hz_Z_file /INPUTS/B0_Z_Hz.nii.gz \
+sh_order 3 \
+symmetry sym \
+coord_geom Philips_headfirst_supine \
+image_radius 135 \
+out_dir /OUTPUTS
+
+
+docker run \
+--mount type=bind,src=`pwd`/INPUTS,dest=/INPUTS \
+--mount type=bind,src=`pwd`/OUTPUTS,dest=/OUTPUTS \
+gradtensor \
+/opt/gradtensor/bin/run_apply_gradtensor_to_b.sh \
+/usr/local/MATLAB/MATLAB_Runtime/v92 \
+Limg_file /INPUTS/L.nii.gz \
+refimg_file /INPUTS/dti.nii.gz \
+bval_file /INPUTS/bval.txt \
+bvec_file /INPUTS/bvec.txt \
+out_dir /OUTPUTS
